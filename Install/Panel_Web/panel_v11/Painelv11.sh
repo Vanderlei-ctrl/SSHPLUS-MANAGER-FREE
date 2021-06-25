8f1ae752dc16b2e1b7d1b48c28561cf84e218302
Vanderlei  Cordeiro <75081980+Vanderlei-ctrl@users.noreply.github.com>
2021-06-24 22:16:21 -0300

    update scripts

--- Install/Panel_Web/panel_v11/Painelv11.sh
+++ Install/Panel_Web/panel_v11/Painelv11.sh
@@ -91,7 +91,7 @@ sed -i "s;suasenha;$senha;g" /var/www/html/pages/system/pass.php > /dev/null 2>&
 fi
 sleep 1
 cd
-wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v11/sshplus.sql > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Vanderlei-ctrl/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v11/sshplus.sql > /dev/null 2>&1
 sleep 1
 if [[ -e "$HOME/sshplus.sql" ]]; then
     mysql -h localhost -u root -p$senha --default_character_set utf8 sshplus < sshplus.sql
